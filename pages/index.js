2bbc1ccf416f6206a0e09c8932b4202841dd4103
viksit <viksit@gmail.com>
2021-04-25 12:29:50 -0700

    add some front page resources

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,42 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>CovidConnect</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          CovidConnect
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          A CovidFYI.in initiative. This site crawls Twitter and shows supply and demand for various resources like Oxygen, beds and ICUs.
         </p>
 
+        <h3> Also see </h3>
+        <a href="" className={styles.card}>
+          <h3>Documentation</h3>
+          <p className={styles.code}>
+            https://covidconnect.vercel.app/api/data?city=delhi&resource_type=demand<br/>
+            https://covidconnect.vercel.app/api/data?city=delhi&resource_type=supply<br/>
+          </p>
+        </a>
+        
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="http://covidfyi.in/" className={styles.card}>
+            <h3>CovidFYI.in</h3>
+            <p>Search resources</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://indiacovidresources.in/" className={styles.card}>
+            <h3>India Covid Resources</h3>
+            <p>Centralized database of resources</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Built by &nbsp;<a href="https://twitter.com/viksit">@viksit</a>. Please reach out to contribute.
       </footer>
     </div>
   )
